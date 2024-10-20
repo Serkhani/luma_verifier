7d718a802c31de64bc74dfc47d4f2eac0ce07e9c
serkhani <dak.boniface@gmail.com>
2024-10-20 11:44:53 +0000

    added changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,31 @@ import { ethers } from "ethers";
 import AttestationABI from "./AttestationABI.json";
 import { Res } from "./lib/types";
 import verifyEvmBasedResult from "./verifyEvmBasedResult";
-
 const FormGrid = ({ children }: { children: React.ReactNode }) => (
-  <div className="grid gap-9 grid-cols-1 max-w-2xl mx-auto my-12">{children}</div>
+  <div className="grid gap-8 grid-cols-1 md:grid-cols-2 max-w-3xl mx-auto my-12 bg-gray-900 p-6 rounded-lg shadow-lg">
+    {children}
+  </div>
 );
 
 const FormContainer = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex flex-col items-center w-full">{children}</div>
+  <div className="flex flex-col items-center w-full  space-y-6 p-4 bg-gray-900">{children}</div>
 );
 
+
 const FormItem = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex flex-col justify-start items-start w-full mb-4">{children}</div>
+  <div className="flex flex-col justify-start items-start w-full mb-6 space-y-2">
+    {children}
+  </div>
 );
 
 const Label = ({ children }: { children: React.ReactNode }) => (
-  <div className="text-right text-lg font-bold text-white mb-2">{children}</div>
+  <label className="text-left text-base font-medium text-gray-300 mb-1">{children}</label>
 );
 
 const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => (
   <input
     {...props}
-    className="block bg-white rounded h-9 leading-9 w-full px-4 outline-none text-black"
+    className="block bg-gray-800 border border-gray-700 rounded-lg h-10 leading-10 w-full px-4 outline-none text-white focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
   />
 );
 
@@ -35,20 +39,21 @@ const Button = ({ children, disabled, onClick }: { children: React.ReactNode, di
   <button
     onClick={onClick}
     disabled={disabled}
-    className={`relative block min-w-[120px] h-9 leading-9 px-4 text-center border-none rounded text-sm bg-[#c5ff4a] text-black ${disabled ? "cursor-not-allowed" : "cursor-pointer"} active:border active:border-gray-400 active:text-gray-700`}
+    className={`relative block min-w-[120px] h-10 leading-10 px-6 text-center border-none rounded-lg text-sm font-semibold bg-green-600 text-white transition-colors duration-300 ${
+      disabled ? "cursor-not-allowed bg-gray-500" : "cursor-pointer hover:bg-green-500 active:bg-green-700"
+    }`}
   >
     {children}
   </button>
 );
 
 const RightContainer = ({ children }: { children: React.ReactNode }) => (
-  <div className="col-span-1">{children}</div>
+  <div className="col-span-1 bg-gray-800 rounded-lg p-6 shadow-lg">{children}</div>
 );
 
 const Title = ({ children }: { children: React.ReactNode }) => (
-  <h2 className="text-white text-center">{children}</h2>
+  <h2 className="text-3xl text-white text-center font-semibold mb-8">{children}</h2>
 );
-
 declare global {
   interface Window {
     ethereum: any;
@@ -57,10 +62,15 @@ declare global {
 
 export default function Home() {
   const [appid1, setAppid1] = useState<string>(
-    "457d0c43-53ea-4d67-8d27-d89174f51bb7"
+    // "457d0c43-53ea-4d67-8d27-d89174f51bb7"
+    // "ed8aa728-ae64-418f-8f7f-447c416033f6"
+    "d8b29079-2fc2-4b3a-8ef0-17a9fd6fab06"
+    // "fc110457-b954-482a-b73d-710316120d2c"
   );
   const [value1, setValue1] = useState<string>(
-    "a9b7a62043a2485cac24ebfe0f03df9a"
+    "1011731a959546e3912de18564bc7abb"
+    // "a9b7a62043a2485cac24ebfe0f03df9a"
+    // "ec0661b638474c16b59621a44951d14a"
   );
   const [result, setResult] = useState<any>();
   const [attestAtationTx, setAttestAtationTx] = useState<string>();
@@ -128,11 +138,11 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <Title>zkPass Transgate JS-SDK Demo(send to evm chain)</Title>
+      <Title>Proof of Participation: iExchange Testing and ZK Workshop</Title>
       <FormGrid>
         <FormContainer>
           <FormItem>
-            <Label>Appid:</Label>
+            <Label>Appid: </Label>
             <Input
               value={appid1}
               onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
@@ -141,7 +151,7 @@ export default function Home() {
             />
           </FormItem>
           <FormItem>
-            <Label>Schema Id:</Label>
+            <Label>SchemaId: </Label>
             <Input
               value={value1}
               onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
@@ -157,7 +167,7 @@ export default function Home() {
           <FormItem>
             {attestAtationTx && (
               <>
-                <Label>AttestationTx:</Label>
+                <Label>AttestationTx: </Label>
                 <a href={"https://explorer-holesky.morphl2.io/tx/" + attestAtationTx} target="_blank" rel="noopener noreferrer">
                   {attestAtationTx}
                 </a>
@@ -165,7 +175,7 @@ export default function Home() {
             )}
             {result && (
               <>
-                <Label>Result:</Label>
+                <Label>Result: </Label>
                 <JSONPretty
                   themeClassName="custom-json-pretty"
                   id="json-pretty"
